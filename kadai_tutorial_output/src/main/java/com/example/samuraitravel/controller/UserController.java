60e89cad8c13959a3ffedb2d6bb83220240c6ab5
yu-sa-1102 <spica-yihsa3@i.softbank.jp>
2025-05-03 17:44:42 +0900

    first edit commit

--- kadai_tutorial_output/src/main/java/com/example/samuraitravel/controller/UserController.java
+++ kadai_tutorial_output/src/main/java/com/example/samuraitravel/controller/UserController.java
@@ -7,6 +7,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import com.example.samuraitravel.entity.User;
+import com.example.samuraitravel.form.UserEditForm;
 import com.example.samuraitravel.repository.UserRepository;
 import com.example.samuraitravel.security.UserDetailsImpl;
 
@@ -27,4 +28,14 @@ public class UserController {
 		
 		return "user/index";
 	}
+	
+	@GetMapping("/edit")
+    public String edit(@AuthenticationPrincipal UserDetailsImpl userDetailsImpl, Model model) {        
+        User user = userRepository.getReferenceById(userDetailsImpl.getUser().getId());  
+        UserEditForm userEditForm = new UserEditForm(user.getId(), user.getName(), user.getFurigana(), user.getPostalCode(), user.getAddress(), user.getPhoneNumber(), user.getEmail());
+        
+        model.addAttribute("userEditForm", userEditForm);
+        
+        return "user/edit";
+    }
 }
